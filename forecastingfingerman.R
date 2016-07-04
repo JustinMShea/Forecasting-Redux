bfadd637108717745eb65e495f11c555c941821f
JustinMShea <jshea@mail.roosevelt.edu>
2016-07-04 17:28:34 -0500

    Pg. 8 Smoothing

--- forecastingfingerman.R
+++ forecastingfingerman.R
@@ -22,8 +22,45 @@ colnames(table_61_series) <- c("Period", "Sales Volume")
 str(table_61_series)
 
 
- #Creat chjart
+ #Creat chart
 library(ggplot2)
-ggplot(data = table_61_series, aes(y = `Sales Volume`, x = Period)) +
-        geom_line()
+plot_61 <- ggplot(data = table_61_series, aes(y = `Sales Volume`, x = Period)) +
+           geom_line() 
+
+
+## page 6, create table 2
+ # Import data from table 6.2 text file
+Table_62_path <- path.expand("~/R/forecasting/Table 6.2.txt")
+table_62 <- read.delim(Table_62_path, header = FALSE, sep = " ", skip = 1)
+ # add column names
+colnames(table_62) <- c("Period", "Actual", "Period", "Actual")
+ # review structure
+str(table_62)
+
+## Page 3, create plot
+# First, gather the time series data table into one column.
+library(tidyr)
+table_62_series <- gather(table_62[,c(2,4)])
+table_62_series$key<- as.numeric(rownames(table_62_series))
+colnames(table_62_series) <- c("Period", "Horizontal Data")
+str(table_62_series)
+
+
+#Creat chart
+library(ggplot2)
+plot_62 <- ggplot(data = table_62_series, aes(y = `Horizontal Data`, x = Period)) +
+        geom_line() 
+
+plot_62
+
+mean_62 <- round(mean(table_62_series[,2]), digits = 1)
+
+plot_62 +  geom_hline(aes(yintercept=356.5), color = "red", linetype="dashed", size = 1.3)
+
+## Smoothing moving average, pg 8.
+
+period_3_example <- table_62_series[6:8,]
+smooth <- round(mean(table_62_series[6:8,2]), digits = 2)
+period_3_example$Smoothing <- c("...",smooth,"...")
+
 
