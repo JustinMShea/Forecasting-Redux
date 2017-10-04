05ca78a9c5a854a684cf89d09556197cd0a9013c
JustinMShea <jshea@mail.roosevelt.edu>
2017-10-04 16:02:06 -0500

    Eviews data scripts.

--- R/deibold-5.R
+++ R/deibold-5.R
@@ -1,12 +1,23 @@
 
+# Data download
+
 library(hexView)
 # C:\Users\skate_more\Documents\R\forecasting\data\Data Sets\fcst4-05
 data_path <- paste0(getwd(),"/data/Data Sets/fcst4-05/fcst5input.wf1")
 fcst5input.wf1 <- readEViews(data_path)
 
 
-data_path_1 <- paste0(getwd(),"/data/Data Sets/fcst4-05/fcst5input.dat")
-fcst5input.dat <- readEViews(data_path_1)
+library(readr)
+fcst5input.dat <- read_delim("~/R/forecasting/data/Data Sets/fcst4-05/fcst5input.dat", 
+                         "\t", escape_double = FALSE, col_types = cols(RTRR = col_integer()), 
+                         trim_ws = TRUE)
+
+PC5_07_EUROstar <- read_csv("~/R/forecasting/data/Data Sets/fcst4-05/PC5_07_EUROstar.dat")
 
 data_path_2 <- paste0(getwd(),"/data/Data Sets/fcst4-05/fcst5finalized.wf1")
 fcst5finalized.wf1 <- readEViews(data_path_2)
+
+## Models
+
+
+
